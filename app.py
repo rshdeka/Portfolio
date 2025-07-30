e07d55d18cdd887a8fc45b42428618ab37258a95
Rajashri Deka <86558414+rshdeka@users.noreply.github.com>
2025-07-30 21:41:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ def main_page():
         # Education
         st.header("🎓 Education")
         st.markdown("""  
-        - **Bachelor of Technology**    
-          National Institute of Technology Silchar | 2019-2023 
+        - ###Bachelor of Technology    
+          **National Institute of Technology Silchar** | 07/2019-05/2023 
           CGPA: 8.68/10
         """)  
 
