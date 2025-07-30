f8e6995713cd44d075783456506b87d091cf220f
Rajashri Deka <86558414+rshdeka@users.noreply.github.com>
2025-07-30 21:45:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def main_page():
         st.markdown("""  
         - **Bachelor of Technology**   
           **National Institute of Technology Silchar** | 07/2019 - 05/2023           
-          ***CGPA: 8.68/10***
+          CGPA: 8.68/10
         """)  
 
     # About Me
